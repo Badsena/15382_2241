7b84b40530c4e00b115a7ebf309dfa8399690aa8
Badsena <sena@amypo.in>
2025-07-09 17:57:40 +0000

    User

---
+++ reactapp/src/components/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.css';
+
+function Dashboard() {
+  return (
+    <div>
+      <p>Welcome to your dashboard! Select an option from the menu to get started.</p>
+    </div>
+  );
+}
+
+export default Dashboard;
